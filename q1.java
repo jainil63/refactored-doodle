196edc391ddc400456ab972f8caacb51a3c47df1
Jainil Gondaliya <jainilgondaliya63@gmail.com>
2026-01-04 18:47:00 +0530

    Add Java program to calculate factorial of 7

---
+++ q1.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        for(int i=1; i<=7; i++) {
+            a = a*i;
+        }
+        System.out.println(a);
+    }
+}
