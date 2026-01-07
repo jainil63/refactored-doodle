27ee21616139e47f7eed1ccfac4344a67ec6af00
Jainil Gondaliya <jainilgondaliya63@gmail.com>
2026-01-07 21:24:17 +0530

    Fix FizzBuzz logic and update initial number

--- q3.java
+++ q3.java
@@ -1,11 +1,11 @@
 class Main {
     public static void main(String[] args) {
-        int num = 5;
-        if (num%3==0) {
-            System.out.println("Fizz");
-        }
-        else if (num %3==0 || num %5==0) {
+        int num = 30;
+        if (num %3==0 && num %5==0) {
             System.out.println("Fizzbuzz");
+        }
+        else if (num%3==0) {
+            System.out.println("Fizz");
         }
         else if (num %5==0) {
             System.out.println("Buzz");
